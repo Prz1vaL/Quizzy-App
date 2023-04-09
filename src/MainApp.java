cde78d0122b6cfae9147dc2dd13eb67201a5c4d4
apm30 <m.ajaypradeep@icloud.com>
2023-04-09 14:42:04 +0100

    reorganising and refactoring before implementation.

---
+++ src/MainApp.java
@@ -0,0 +1,8 @@
+import View.CommandLineInterface;
+
+public class MainApp {
+    public static void main(String[] args) {
+        CommandLineInterface cli = new CommandLineInterface();
+        cli.run();
+    }
+}
---
+++ src/View/CommandLineInterface.java
@@ -0,0 +1,22 @@
+package View;
+
+import Controller.QuizzyController;
+
+public class CommandLineInterface {
+
+    QuizzyController quizzyController = new QuizzyController();
+
+    public void run() {
+        System.out.println("Hello, User! Welcome to Quizzy!");
+        logo();
+    }
+
+    private void logo() {
+        System.out.println("  _______ _   _ _______ ______ _____  ______ _______ ");
+        System.out.println(" |__   __| \\ | |__   __|  ____|  __ \\|  ____|__   __|");
+        System.out.println("    | |  |  \\| |  | |  | |__  | |__) | |__     | |   ");
+        System.out.println("    | |  | . ` |  | |  |  __| |  _  /|  __|    | |   ");
+        System.out.println("    | |  | |\\  |  | |  | |____| | \\ \\| |____   | |   ");
+        System.out.println("    |_|  |_| \\_|  |_|  |______|_|  \\_\\______|  |_|   ");
+    }
+}
