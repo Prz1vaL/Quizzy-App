24b755ae905a84c90a8ceba086d0cb66acd5a8b5
apm30 <m.ajaypradeep@icloud.com>
2023-04-11 12:40:52 +0100

    added introMessage..

--- src/main/java/Quizzy/View/CommandLineInterface.java
+++ src/main/java/Quizzy/View/CommandLineInterface.java
@@ -2,13 +2,18 @@ package main.java.Quizzy.View;
 
 import main.java.Quizzy.Controller.QuizzyController;
 
+import java.util.Scanner;
+
 public class CommandLineInterface {
 
     QuizzyController quizzyController = new QuizzyController();
 
+    private Scanner scanner;
+
     public void run() {
         System.out.println("Hello, User! Welcome to Quizzy!");
         logo();
+        start();
     }
 
     private void logo() {
@@ -21,4 +26,23 @@ public class CommandLineInterface {
                 "                                                        1.0      \n";
         System.out.println(logo);
     }
+    private void start() {
+        Scanner scanner = new Scanner(System.in);
+        introMessage();
+        String line = scanner.nextLine();
+       //TODO : WHILE LOOP TO LOOP THE CHOICES AND THE SYSTEM DOES NOT CRASH.
+
+    }
+
+    private void introMessage() {
+        System.out.println("***************************************");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Save Data");
+        System.out.println("4. Exit");
+        System.out.println("Please select an option: ");
+        System.out.println("***************************************");
+    }
+
+
 }
