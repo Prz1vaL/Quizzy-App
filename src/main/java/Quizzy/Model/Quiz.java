527ccecd8ed60067270a8398349a51d22e6cd46b
apm30 <apm30>
2023-04-14 10:46:20 +0100

    Created a Quiz Model to hold data for Quizzes (not final version , may subject to change).

---
+++ src/main/java/Quizzy/Model/Quiz.java
@@ -0,0 +1,20 @@
+package main.java.Quizzy.Model;
+
+import java.util.Date;
+
+public class Quiz {
+
+    private int quizID;
+
+    private String quizName;
+
+    private Date dateCreated;
+
+    private String createdByUser;
+
+    private Date dateModified;
+
+    private String lastModifiedBy;
+
+
+}
