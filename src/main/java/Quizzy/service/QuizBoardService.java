5cd1c4cc4be29cbc7c5d60b0039a079c3889987b
apm30 <m.ajaypradeep@icloud.com>
2023-04-23 14:06:15 +0100

    Adding Javadoc.

--- src/main/java/Quizzy/service/QuizBoardService.java
+++ src/main/java/Quizzy/service/QuizBoardService.java
@@ -5,13 +5,26 @@ import main.java.Quizzy.model.QuizBoard;
 import java.io.*;
 import java.util.*;
 
+/**
+ * QuizBoardService class.
+ * This class is responsible for storing the quiz board data
+ * and providing the necessary methods to access and modify the data.
+ */
 public class QuizBoardService implements Serializable {
 
+    // This map stores the quiz board data.
+
     private Map<Integer, QuizBoard> quizBoardMap = new HashMap<>();
+
+    // This list stores the quiz IDs of the quiz boards.
 
     private ArrayList<Integer> quizIDS = new ArrayList<>();
 
 
+    /**
+     * This method saves the quiz board data to a file.
+     * @throws IOException if an I/O error occurs
+     */
     public void saveData() throws IOException {
         FileOutputStream f = new FileOutputStream("src/resources/QuizBoardData.ser");
         ObjectOutputStream o = new ObjectOutputStream(f);
@@ -20,6 +33,11 @@ public class QuizBoardService implements Serializable {
         f.close();
     }
 
+    /**
+     * This method loads the quiz board data from a file.
+     * @throws IOException if an I/O error occurs
+     * @throws ClassNotFoundException if the class of a serialized object cannot be found
+     */
     public void loadData() throws IOException, ClassNotFoundException {
         FileInputStream fi = new FileInputStream("src/resources/QuizBoardData.ser");
         ObjectInputStream oi = new ObjectInputStream(fi);
@@ -30,11 +48,20 @@ public class QuizBoardService implements Serializable {
     }
 
 
+    /**
+     * This method creates a quiz board.
+     * @param quizBoardName The name of the quiz board.
+     * @param courseName The name, of course.
+     * @param dateCreated The date the quiz board was created.
+     * @param createdBy The teacher who created the quiz board.
+     * @return The ID of the quiz board.
+     */
     public int createQuizBoard(String quizBoardName, String courseName, Date dateCreated, String createdBy) {
         // Check for empty string and null
         if (quizBoardName == null || quizBoardName.isEmpty()) {
             throw new IllegalArgumentException("Fields cannot be empty");
         }
+        // Generate a random quiz ID and checking if it is unique or not.
         Random random = new Random();
         int quizID = random.nextInt(1000000);
         // CHECK IF QUIZ ID IS UNIQUE
@@ -56,6 +83,12 @@ public class QuizBoardService implements Serializable {
     }
 
 
+    /**
+     * This method views the quiz board by course.
+     * @param courseName The name, of course.
+     * @param createdBy The teacher who created the quiz board.
+     * @return A map of quiz boards that belong to the course.
+     */
     public Map<Integer, QuizBoard> viewQuizBoardsByCourse(String courseName, String createdBy) {
         // Check for empty string and null
         if (courseName == null || courseName.isEmpty()) {
@@ -70,6 +103,11 @@ public class QuizBoardService implements Serializable {
         return quizBoardMapByCourse;
     }
 
+    /**
+     * This method views all the quiz boards created by the teacher.
+     * @param createdBy The teacher who created the quiz board.
+     * @return A map of all the quiz boards created by the teacher.
+     */
     public Map<Integer, QuizBoard> viewAllQuizBoards(String createdBy) {
         Map<Integer, QuizBoard> quizBoardMapByTeacher = new HashMap<>();
         for (Map.Entry<Integer, QuizBoard> entry : quizBoardMap.entrySet()) {
@@ -80,6 +118,10 @@ public class QuizBoardService implements Serializable {
         return quizBoardMapByTeacher;
     }
 
+    /**
+     * This method deletes a quiz board.
+     * @param quizBoardID The ID of the quiz board.
+     */
     public void deleteQuizBoard(String quizBoardID) {
         if (quizBoardMap.isEmpty() && quizIDS.isEmpty()) {
             throw new IllegalArgumentException("No Quiz Boards exist");
@@ -97,6 +139,12 @@ public class QuizBoardService implements Serializable {
         }
     }
 
+    /**
+     * This method is used for viewing a quiz board by its ID.
+     * @param teacherUserName The teacher who created the quiz board.
+     * @param quizBoardID The ID of the quiz board.
+     * @return A map of the quiz board.
+     */
     public Map<Integer, QuizBoard> viewQuizBoardByQuizBoardID(String teacherUserName, String quizBoardID) {
         Map<Integer, QuizBoard> quizBoardMapByQuizBoardID = new HashMap<>();
         if (quizBoardMap.containsKey(Integer.parseInt(quizBoardID))) {
@@ -111,6 +159,12 @@ public class QuizBoardService implements Serializable {
         return quizBoardMapByQuizBoardID;
     }
 
+    /**
+     * This method is used for editing (Add/delete questions) to a quiz board by its ID.
+     * @param userName The teacher who created the quiz board.
+     * @param quizBoardID The ID of the quiz board.
+     * @return A map of the quiz board.
+     */
     public Map<Integer, QuizBoard> editQuizBoard(String userName, String quizBoardID) {
         Map<Integer, QuizBoard> quizBoardMapByQuizBoardID = new HashMap<>();
         if (quizBoardMap.containsKey(Integer.parseInt(quizBoardID))) {
@@ -126,6 +180,10 @@ public class QuizBoardService implements Serializable {
     }
 
 
+    /**
+     * This method is used for updating the number of questions in a quiz board.
+     * @param quizBoardID The ID of the quiz board.
+     */
     public void updateQuizBoardQuestion(int quizBoardID) {
         if (quizBoardMap.containsKey(quizBoardID)) {
             quizBoardMap.get(quizBoardID).setNumberOfQuestions(quizBoardMap.get(quizBoardID).getNumberOfQuestions() + 1);
@@ -134,6 +192,10 @@ public class QuizBoardService implements Serializable {
         }
     }
 
+    /**
+     * This method is used for deleting a question in a quiz board.
+     * @param quizBoardID The ID of the quiz board.
+     */
     public void deleteQuizBoardQuestion(int quizBoardID) {
         if (quizBoardMap.containsKey(quizBoardID)) {
             quizBoardMap.get(quizBoardID).setNumberOfQuestions(quizBoardMap.get(quizBoardID).getNumberOfQuestions() - 1);
@@ -142,6 +204,11 @@ public class QuizBoardService implements Serializable {
         }
     }
 
+    /**
+     * This method is used for getting the course name of a quiz board.
+     * @param quizBoardID The ID of the quiz board.
+     * @return The course name of the quiz board.
+     */
     public String getQuizBoardCourse(int quizBoardID) {
         if (quizBoardMap.containsKey(quizBoardID)) {
             return quizBoardMap.get(quizBoardID).getCourseName();
@@ -150,6 +217,11 @@ public class QuizBoardService implements Serializable {
         }
     }
 
+    /**
+     * This method is used for getting the name of a quiz board.
+     * @param quizBoardID The ID of the quiz board.
+     * @return The name of the quiz board.
+     */
     public String getQuizBoardName(int quizBoardID) {
         if (quizBoardMap.containsKey(quizBoardID)) {
             return quizBoardMap.get(quizBoardID).getQuizBoardName();
@@ -158,6 +230,12 @@ public class QuizBoardService implements Serializable {
         }
     }
 
+    /**
+     * This method is used for updating the student score to the quiz board.
+     * @param studentName The name of the student.
+     * @param quizBoardID The ID of the quiz board.
+     * @param studentScore The score of the student.
+     */
     public void updateStudentScoreBoard(String studentName, int quizBoardID, float studentScore) {
         if (quizBoardMap.containsKey(quizBoardID)) {
             quizBoardMap.get(quizBoardID).getStudentScores().put(studentName.toLowerCase(), studentScore);
@@ -168,6 +246,11 @@ public class QuizBoardService implements Serializable {
 
     }
 
+    /**
+     * This method is used for checking if a quiz has been taken by a student.
+     * @param userName The name of the student.
+     * @param quizBoardID   Quiz Board ID.
+     */
     public void checkIfQuizTaken(String userName, int quizBoardID) {
         if (quizBoardMap.containsKey(quizBoardID)) {
             if (quizBoardMap.get(quizBoardID).getStudentScores().containsKey(userName.toLowerCase())) {
@@ -178,6 +261,11 @@ public class QuizBoardService implements Serializable {
         }
     }
 
+    /**
+     * This method is used for getting the total score of a quiz board.
+     * @param quizBoardID  The ID of the quiz board.
+     * @return The students' score who has taken the quiz.
+     */
     public Map<String, Float> viewTeacherQuizResults(int quizBoardID) {
         if (quizBoardMap.containsKey(quizBoardID)) {
             return quizBoardMap.get(quizBoardID).getStudentScores();
